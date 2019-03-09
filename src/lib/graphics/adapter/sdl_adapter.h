eba6f3499ccf430a21d52e9b220ea568466b5b99
Andrey Feofilaktov <andarpolis@gmail.com>
2019-03-10 02:14:07 +0300

    buildable but not working version with imgui used

--- src/lib/graphics/adapter/sdl_adapter.h
+++ src/lib/graphics/adapter/sdl_adapter.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <external/gl/gl_core_3_3.h>
+
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <external/imgui/imgui.h>
 
 namespace graphics {
     class SDL {
@@ -23,5 +25,9 @@ namespace graphics {
 
         int context_major_version_;
         int context_minor_version_;
+
+        bool show_demo_window_ = true;
+        bool show_another_window_ = false;
+        ImVec4 clear_color_ = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     };
 }
