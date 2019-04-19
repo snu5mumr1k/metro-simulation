b9a82ab475e7eaa3e1b27fbe61cd4817108b0154
snusmumrik <andarpolis@gmail.com>
2019-04-19 13:36:52 +0300

    add-remove functionality for stations and sections

--- src/lib/graphics/interface/metro_editor.cpp
+++ src/lib/graphics/interface/metro_editor.cpp
@@ -15,40 +15,62 @@ metro_simulation::Metro EditMetro(metro_simulation::Metro metro) {
         const auto &line = metro.lines(i);
         max_line_id = std::max(max_line_id, line.id());
 
-        ImGui::Button("-");
+        if (ImGui::Button("-##line")) {
+            metro.mutable_lines()->SwapElements(i, metro.lines().size() - 1);
+            metro.mutable_lines()->RemoveLast();
+            i--;
+            continue;
+        }
         ImGui::SameLine();
         const std::string line_header = "Line " + std::to_string(line.id());
         if (ImGui::CollapsingHeader(line_header.c_str())) {
             ImGui::Indent();
-            {
-                if (line.stations().size() > 0) {
-                    if (ImGui::CollapsingHeader("Stations")) {
-                        for (const auto &station : line.stations()) {
-                            ImGui::Button("-");
-                            ImGui::SameLine();
-                            ImGui::Text("Id %lld", station.id());
-                            for (const auto &platform : station.platforms()) {
-                                ImGui::BulletText("Platform %lld", platform.id());
-                            }
-                        }
+            if (ImGui::CollapsingHeader("Stations")) {
+                int64_t max_station_id = -1;
+                for (int j = 0; j < line.stations().size(); ++j) {
+                    const auto &station = line.stations(j);
+                    max_station_id = std::max(max_station_id, station.id());
+
+                    if (ImGui::Button("-##station")) {
+                        auto stations = metro.mutable_lines(i)->mutable_stations();
+                        stations->SwapElements(j, stations->size() - 1);
+                        stations->RemoveLast();
+                        j--;
+                        continue;
                     }
-                } else {
-                    ImGui::Button("+");
                     ImGui::SameLine();
-                    ImGui::Text("No stations");
+                    ImGui::Text("Station %lld", station.id());
+                    ImGui::Indent();
+                    for (const auto &platform : station.platforms()) {
+                        ImGui::BulletText("Platform %lld", platform.id());
+                    }
+                    ImGui::Unindent();
                 }
-                if (line.sections().size() > 0) {
-                    if (ImGui::CollapsingHeader("Sections")) {
-                        for (const auto &section : line.sections()) {
-                            ImGui::Button("-");
-                            ImGui::SameLine();
-                            ImGui::Text("Id %lld", section.id());
-                        }
+                if (ImGui::Button("+##station")) {
+                    auto new_station = metro.mutable_lines(i)->add_stations();
+                    new_station->set_id(max_station_id + 1);
+                    new_station->add_platforms()->set_id(0);
+                    new_station->add_platforms()->set_id(1);
+                }
+            }
+            if (ImGui::CollapsingHeader("Sections")) {
+                int64_t max_section_id = -1;
+                for (int j = 0; j < line.sections().size(); ++j) {
+                    const auto &section = line.sections(j);
+                    max_section_id = std::max(max_section_id, section.id());
+                    if (ImGui::Button("-##section")) {
+                        auto sections = metro.mutable_lines(i)->mutable_sections();
+                        sections->SwapElements(j, line.sections().size() - 1);
+                        sections->RemoveLast();
+                        j--;
+                        continue;
                     }
-                } else {
-                    ImGui::Button("+");
                     ImGui::SameLine();
-                    ImGui::Text("No sections");
+                    ImGui::Text("Section %lld", section.id());
+                }
+                if (ImGui::Button("+##section")) {
+                    auto new_section = metro.mutable_lines(i)->add_sections();
+                    new_section->set_id(max_section_id + 1);
                 }
             }
             ImGui::Unindent();
@@ -57,7 +79,7 @@ metro_simulation::Metro EditMetro(metro_simulation::Metro metro) {
         ImGui::Separator();
     }
 
-    if (ImGui::Button("+")) {
+    if (ImGui::Button("+##line")) {
         auto new_line = metro.add_lines();
         new_line->set_id(max_line_id + 1);
     }
