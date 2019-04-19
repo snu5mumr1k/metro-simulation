9cd24c2c2da8fdde0bf161014d868e79f7bb39a2
snusmumrik <andarpolis@gmail.com>
2019-04-19 11:32:58 +0300

    rearrange buttons

--- src/lib/graphics/interface/metro_editor.cpp
+++ src/lib/graphics/interface/metro_editor.cpp
@@ -6,71 +6,60 @@
 
 namespace graphics {
 metro_simulation::Metro EditMetro(metro_simulation::Metro metro) {
-    static bool currently_editing_line = false;
-    static int64_t new_line_index = -1;
-
     ImGui::Begin("Edit Metro");
 
-    if (currently_editing_line) {
-        ImGui::Text("Line id: %lld", metro.lines(new_line_index).id());
+    ImGui::Text("Lines");
 
-        if (ImGui::Button("Save")) {
-            currently_editing_line = false;
-        }
+    int64_t max_line_id = -1;
+    for (int i = 0; i < metro.lines().size(); ++i) {
+        const auto &line = metro.lines(i);
+        max_line_id = std::max(max_line_id, line.id());
+
+        ImGui::Button("-");
         ImGui::SameLine();
-        if (ImGui::Button("Delete")) {
-            auto lines = metro.mutable_lines();
-            lines->SwapElements(new_line_index, metro.lines().size() - 1);
-            lines->RemoveLast();
-            currently_editing_line = false;
-        }
-    } else {
-        ImGui::Text("Lines");
-
-        int64_t max_line_id = -1;
-        std::vector<int64_t> to_delete;
-        for (int i = 0; i < metro.lines().size(); ++i) {
-            const auto &line = metro.lines(i);
-            max_line_id = std::max(max_line_id, line.id());
-
-            const std::string line_header = "Line " + std::to_string(line.id());
-            if (ImGui::CollapsingHeader(line_header.c_str())) {
-                ImGui::Indent();
-                {
+        const std::string line_header = "Line " + std::to_string(line.id());
+        if (ImGui::CollapsingHeader(line_header.c_str())) {
+            ImGui::Indent();
+            {
+                if (line.stations().size() > 0) {
                     if (ImGui::CollapsingHeader("Stations")) {
                         for (const auto &station : line.stations()) {
+                            ImGui::Button("-");
+                            ImGui::SameLine();
                             ImGui::Text("Id %lld", station.id());
                             for (const auto &platform : station.platforms()) {
                                 ImGui::BulletText("Platform %lld", platform.id());
                             }
                         }
                     }
+                } else {
+                    ImGui::Button("+");
+                    ImGui::SameLine();
+                    ImGui::Text("No stations");
+                }
+                if (line.sections().size() > 0) {
                     if (ImGui::CollapsingHeader("Sections")) {
                         for (const auto &section : line.sections()) {
+                            ImGui::Button("-");
+                            ImGui::SameLine();
                             ImGui::Text("Id %lld", section.id());
                         }
                     }
+                } else {
+                    ImGui::Button("+");
+                    ImGui::SameLine();
+                    ImGui::Text("No sections");
                 }
-                if (!currently_editing_line) {
-                    currently_editing_line = ImGui::Button("Edit");
-                    if (currently_editing_line) {
-                        new_line_index = i;
-                    }
-                }
-                ImGui::Unindent();
             }
+            ImGui::Unindent();
         }
 
         ImGui::Separator();
+    }
 
-        if (!currently_editing_line) {
-            currently_editing_line = ImGui::Button("Add new line");
-            if (currently_editing_line) {
-                auto new_line = metro.add_lines();
-                new_line->set_id(max_line_id + 1);
-                new_line_index = metro.lines().size() - 1;
-            }
-        }
+    if (ImGui::Button("+")) {
+        auto new_line = metro.add_lines();
+        new_line->set_id(max_line_id + 1);
     }
 
     ImGui::End();
